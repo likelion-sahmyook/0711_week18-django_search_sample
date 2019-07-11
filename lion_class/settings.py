7e8c4e285ec02b2bef7e3098e36e6046b813c2dd
seonghyeon <tjdgusdkf@gmail.com>
2019-07-11 17:47:51 +0900

    bsh

--- lion_class/settings.py
+++ lion_class/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'lion_class.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['blog/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
